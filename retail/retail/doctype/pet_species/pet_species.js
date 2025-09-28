20b51660b6d658d5ec4dfac439516a38e785b627
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-09-28 14:34:46 +0300

    grooming appintments

---
+++ retail/retail/doctype/pet_species/pet_species.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Projecx Team and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pet Species", {
+// 	refresh(frm) {
+
+// 	},
+// });
