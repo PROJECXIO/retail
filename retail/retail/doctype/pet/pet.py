c362f06e428317534b804547c7e8371a655b2cf9
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-09-28 16:42:48 +0300

    pet vacciens

--- retail/retail/doctype/pet/pet.py
+++ retail/retail/doctype/pet/pet.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, Projecx Team and contributors
 # For license information, please see license.txt
+from datetime import datetime
 
 # import frappe
 from frappe.model.document import Document
 
 
 class Pet(Document):
-	pass
+	def validate(self):
+		if len(self.pet_vaccinations) == 0:
+			return
+		vaccine_exp_dates = sorted([pv.expiration_date for pv in self.pet_vaccinations if pv.expiration_date], key=lambda d: datetime.strptime(d, "%Y-%m-%d"))
+		if len(vaccine_exp_dates) == 0:
+			return
+		self.last_vaccine_exp_date = vaccine_exp_dates[-1]
