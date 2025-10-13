8f232b2ec8ffd2ebcb8eb11ee5b3e6e93a5085a5
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-10-13 10:38:49 +0300

    enable discount in pet services

--- retail/retail/doctype/pet_service/pet_service.js
+++ retail/retail/doctype/pet_service/pet_service.js
@@ -3,20 +3,62 @@
 
 frappe.ui.form.on("Pet Service", {
     update_total_price(frm) {
-        let total_price = (frm.doc.service_items || []).reduce(
-            (prev, curr) => prev + flt(curr.rate),
-            0
-        );
+        let total_price = 0;
+        let total_net_price = 0;
+        (frm.doc.service_items || []).forEach(row => {
+            total_price += flt(row.rate);
+            if(row.discount_as == "Percent"){
+                total_net_price += (flt(row.rate) - (flt(row.rate) * flt(row.discount) / 100));
+            } else if(row.discount_as == "Fixed Amount"){
+                total_net_price += (flt(row.rate) - flt(row.discount));
+            } else {
+                total_net_price += flt(row.rate);
+            }
+        });
+
         frm.set_value("total_price", total_price);
         frm.refresh_field("total_price");
+
+        frm.set_value("total_net_price", total_net_price);
+        frm.refresh_field("total_net_price");
+    },
+    validate(frm){
+        (frm.doc.service_items || []).forEach(row => {
+            if(row.item_code && row.discount_as == "Percent" && flt(row.discount) > 100){
+                frappe.throw(__("Discount Percent at row {} can not be greater that 100", [row.idx]))
+            }
+            if(row.item_code && row.discount_as == "Fixed Amount" && flt(row.discount) >  flt(row.rate)){
+                frappe.throw(__("Discount Amount at row {} can not be greater that rate {}", [row.idx, row.rate]))
+            }
+        });
     },
 });
 
 frappe.ui.form.on("Service Item", {
-    service_items_remove(frm, cd, cn) {
+    service_items_remove(frm, cdt, cdn) {
         frm.trigger("update_total_price");
     },
-    rate(frm, cd, cn) {
+    discount(frm, cdt, cdn) {
         frm.trigger("update_total_price");
+    },
+    discount_as(frm, cdt, cdn) {
+        frm.trigger("update_total_price");
+    },
+    async item_code(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.item_code) {
+            const value = await frappe.db.get_value(
+                "Item Price",
+                { item_code: row.item_code, uom: row.uom, selling: 1 },
+                "price_list_rate"
+            );
+            const rate = (value && value.message && value.message.price_list_rate) || 0;
+            row.rate = rate;
+            frm.trigger("update_total_price");
+        }else{
+            row.rate = 0;
+            frm.trigger("update_total_price");
+        }
+        
     },
 });
--- retail/retail/doctype/pet_service/pet_service.py
+++ retail/retail/doctype/pet_service/pet_service.py
@@ -8,5 +8,13 @@ from frappe.utils import flt
 class PetService(Document):
 	def validate(self):
 		self.total_price = 0
+		self.total_net_price = 0
 		for item in self.service_items:
 			self.total_price += flt(item.rate)
+			if item.discount_as == "Percent":
+				self.total_net_price += (flt(item.rate) - (flt(item.rate) * flt(item.discount) / 100));
+			elif item.discount_as == "Fixed Amount":
+				self.total_net_price += (flt(item.rate) - flt(item.discount));
+			else:
+				self.total_net_price += flt(item.rate);
+
