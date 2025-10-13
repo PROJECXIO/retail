77b6ad07eb72f4e622f677cf37f85407fd04673c
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-10-13 12:21:40 +0300

    fix validation and totals

--- retail/retail/doctype/pet_service/pet_service.py
+++ retail/retail/doctype/pet_service/pet_service.py
@@ -1,20 +1,33 @@
 # Copyright (c) 2025, Projecx Team and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 from frappe.utils import flt
 
 class PetService(Document):
 	def validate(self):
-		self.total_price = 0
-		self.total_net_price = 0
+		total_price, total_net_price = self.check_discount_values()
+		self.total_price = total_price
+		self.total_net_price = total_net_price
+	
+	def check_discount_values(self):
+		total_price = 0
+		total_net_price = 0
 		for item in self.service_items:
-			self.total_price += flt(item.rate)
+			if item.discount_as == "Percent" and flt(item.discount) > 100:
+				frappe.throw(_("Discount Percent at row {} can not be greater that 100").format(item.idx))
+			if item.discount_as == "Fixed Amount" and flt(item.discount) >  flt(item.rate):
+				frappe.throw(_("Discount Amount at row {} can not be greater that rate {}").format(item.idx, item.rate))
+			
+			total_price += flt(item.rate)
 			if item.discount_as == "Percent":
-				self.total_net_price += (flt(item.rate) - (flt(item.rate) * flt(item.discount) / 100));
+				total_net_price += (flt(item.rate) - (flt(item.rate) * flt(item.discount) / 100))
 			elif item.discount_as == "Fixed Amount":
-				self.total_net_price += (flt(item.rate) - flt(item.discount));
+				total_net_price += (flt(item.rate) - flt(item.discount))
 			else:
-				self.total_net_price += flt(item.rate);
+				total_net_price += flt(item.rate)
+			
+		return total_price, total_net_price
 
