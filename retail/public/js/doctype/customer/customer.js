df73a336340ae35ffb5865719e355e71751fadce
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-10-29 16:09:37 +0300

    refresh map

--- retail/public/js/doctype/customer/customer.js
+++ retail/public/js/doctype/customer/customer.js
@@ -1,14 +1,14 @@
 frappe.ui.form.on("Customer", {
-    async onload(frm){
-        const pets = await frappe.db.get_list("Pet", { filters: {"customer": frm.doc.name}, fields: ["*"] }) || [];
+    async onload(frm) {
+        const pets = await frappe.db.get_list("Pet", { filters: { "customer": frm.doc.name }, fields: ["*"] }) || [];
         const pets_profile = pets.map(pet => {
             let color = "red";
-            if(pet.last_vaccine_exp_date){
+            if (pet.last_vaccine_exp_date) {
                 var date1 = new Date(pet.last_vaccine_exp_date);
                 var date2 = new Date();
                 var diffTime = date1.getTime() - date2.getTime();
                 var diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
-                if(diffDays > 7){
+                if (diffDays > 7) {
                     color = "green";
                 }
             }
@@ -19,12 +19,18 @@ frappe.ui.form.on("Customer", {
                 <h3 class="pet-name">${pet.pet_name}</h3>
                 <p class="pet-type">${__("Type")}: ${pet.pet_type}</p>
                 <p class="pet-dob">${__("Date of Birth")}: ${pet.date_of_birth || 'N/A'}</p>
-                <p class="pet-vaccine">${__("Vaccine Expire")}: <span class="${color}">${pet.last_vaccine_exp_date }</span></p>
+                <p class="pet-vaccine">${__("Vaccine Expire")}: <span class="${color}">${pet.last_vaccine_exp_date}</span></p>
                 </div>
             </a>`}).join("");
         $(frm.fields_dict['custom_customer_pets_profile'].wrapper).html(`
             <div class="pet-list">${pets_profile}</div>
-            `)
-        
+            `);
+        frm.$wrapper.find('.form-tabs .nav-link').on('shown.bs.tab', function (e) {
+            const newTab = $(e.target).text().trim();
+            if (newTab === 'Address & Contact') {
+                frm.refresh_field("custom_pin_location");
+            }
+            console.log("Switched to tab:", newTab);
+        });
     },
 });
