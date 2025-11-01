93000dc3cda43484a9c3b6a93463705d218b5d52
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-01 16:17:41 +0300

    update customers contacts

---
+++ retail/patches/v0_0/update_customers_contacts.py
@@ -0,0 +1,15 @@
+import frappe
+
+def execute():
+    for c in frappe.db.sql("SELECT name, mobile_no, email_id, custom_mobile_no_2, custom_email_id_2 FROM `tabCustomer`"):
+        name = c[0]
+        mobile_no = c[1]
+        email_id = c[2]
+        mobile_no_2 = c[3]
+        email_id_2 = c[4]
+
+        if mobile_no and not mobile_no_2:
+            frappe.db.set_value("Customer", name, "custom_mobile_no_2", mobile_no)
+        if email_id and not email_id_2:
+            frappe.db.set_value("Customer", name, "custom_email_id_2", email_id)
+    frappe.db.commit()
