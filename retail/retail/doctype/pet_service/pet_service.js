db3416707fbbe113286b9640b656226d4d17499b
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-05 13:18:12 +0300

    update pet service structure

--- retail/retail/doctype/pet_service/pet_service.js
+++ retail/retail/doctype/pet_service/pet_service.js
@@ -1,31 +1,5 @@
 // Copyright (c) 2025, Projecx Team and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Pet Service", {
-    update_total_price(frm) {
-        const price = (frm.doc.service_items || []).reduce((prev, curr) => prev + flt(curr.rate), 0);
-        frm.set_value("total_price", price);
-        frm.refresh_field("total_price");
-
-        frm.set_value("selling_price", price);
-        frm.refresh_field("selling_price");
-    },
-});
-
-frappe.ui.form.on("Pet Service Item Detail", {
-    service_items_remove(frm, cdt, cdn) {
-        frm.trigger("update_total_price");
-    },
-    async pet_service_item(frm, cdt, cdn) {
-        const row = locals[cdt][cdn];
-        if (row.pet_service_item) {
-            frm.trigger("update_total_price");
-        } else {
-            row.rate = 0;
-            frm.trigger("update_total_price");
-        }
-    },
-    rate(frm, cdt, cdn) {
-        frm.trigger("update_total_price");
-    },
-});
+// frappe.ui.form.on("Pet Service", {
+// });
--- retail/retail/doctype/pet_service/pet_service.py
+++ retail/retail/doctype/pet_service/pet_service.py
@@ -3,25 +3,18 @@
 
 # import frappe
 from frappe.model.document import Document
-from frappe.utils import flt
 
 
 class PetService(Document):
     def validate(self):
-        self.total_price, self.pet_type, self.pet_size = self.check_discount_values()
-        if flt(self.selling_price) <= 0:
-            self.selling_price = flt(self.total_price)
+        self.pet_type, self.pet_size = self.update_pet_specifications()
 
-        self.different_price = flt(self.selling_price) - flt(self.total_price)
-
-    def check_discount_values(self):
+    def update_pet_specifications(self):
 
         type = set()
         size = set()
-        total_price = 0
     
         for row in self.service_items:
-            total_price += flt(row.rate)
             if row.pet_type:
                 type.add(row.pet_type)
             if row.pet_size:
@@ -29,4 +22,4 @@ class PetService(Document):
         size = ", ".join(size)
         type = ", ".join(type)
 
-        return total_price, type, size
+        return type, size
