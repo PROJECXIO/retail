dc2228dd6cf900414a0c397511c12b6db0b0eb01
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-12 12:59:29 +0300

    setup tips and commissions

--- retail/retail/doctype/appointment_commission/appointment_commission.py
+++ retail/retail/doctype/appointment_commission/appointment_commission.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, Projecx Team and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import cint, flt
 
 
 class AppointmentCommission(Document):
-    pass
+    def validate(self):
+        self.validate_commissions_and_tips()
+
+    def validate_commissions_and_tips(self):
+        if cint(self.enabled) == 0:
+            return
+        # Validate commissions totals
+        total_commissions = (
+            flt(self.groomer_commission)
+            + flt(self.driver_commission)
+            + flt(self.other_commission)
+        )
+        if total_commissions > 100:
+            frappe.throw(_("Total commissions % can not be greater than 100"))
+        total_tips = (
+            flt(self.groomer_tips) + flt(self.driver_tips) + flt(self.other_tips)
+        )
+        if total_tips > 100:
+            frappe.throw(_("Total tips % can not be greater than 100"))
