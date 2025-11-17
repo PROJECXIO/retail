5baa55bc81bb3f7d1bc41934eb06a1c23e91d269
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-17 15:45:07 +0300

    stop endpoint overrideing

--- retail/api/controllers/booking.py
+++
@@ -1,4 +0,0 @@
-from retail.api.utils.endpoints import create_doc
-def create_booking():
-    doctype = "Booking Request"
-    create_doc(doctype, ignore_permissions=True)
--- retail/api/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from retail.api.routes.booking import booking_rules
-from retail.api.routes.pet_service import service_rules
-url_rules = []
-
-url_rules += booking_rules
-url_rules += service_rules
