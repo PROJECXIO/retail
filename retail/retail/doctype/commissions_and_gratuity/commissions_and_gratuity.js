3d14a9b10febeec437cbfeffd55e2b471ec2b0fd
Abdallah A. Zaqout <abdualla1@students.iugaza.edu.ps>
2025-11-18 10:51:26 +0300

    rename doctype

--- retail/retail/doctype/commissions_and_gratuity/commissions_and_gratuity.js
+++ retail/retail/doctype/commissions_and_gratuity/commissions_and_gratuity.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Projecx Team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Appointment Commission", {
+// frappe.ui.form.on("Commissions and Gratuity", {
 // 	refresh(frm) {
 
 // 	},
